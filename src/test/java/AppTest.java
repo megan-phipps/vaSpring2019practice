47da5a0e34cf08c2a5855c5020c150e74615b40c
megan-phipps <meganphipps7@gmail.com>
2019-08-06 20:17:57 -0400

    hi

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,8 +1,6 @@
 public class AppTest {
     public static void main(String[] args) {
-        System.out.println("Hello");
-        System.out.println("Hello");
-        System.out.println("Hello");
+        System.out.println("Hi");
     }
 }
 
