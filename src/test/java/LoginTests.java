c97645b03f9a49e75337654d0515a7c008c8b683
megan-phipps <meganphipps7@gmail.com>
2019-08-06 21:26:28 -0400

    line 5

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -2,7 +2,10 @@ public class LoginTests {
 
     public static void main(String[] args) {
         System.out.println("Line 1");
-
+        System.out.println("Line 2");
+        System.out.println("Line 3");
+        System.out.println("Line 4");
+        System.out.println("Line 5");
     }
 
 
